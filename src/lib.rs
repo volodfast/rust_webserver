74ae4bfd138b04bca922501db05882cd8e60dded
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-12-10 23:21:04 +0200

    Implement sending and receiving closure functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
 use std::thread;
 
 struct Worker {
@@ -6,15 +9,24 @@ struct Worker {
 }
 
 impl Worker {
-  fn new(id: usize) -> Self {
-    let thread = thread::spawn(|| {});
+  fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Self {
+    let thread = thread::spawn(move || loop {
+      let job = receiver.lock().unwrap().recv().unwrap();
+
+      println!("Worker {} got a job; executing.", id);
+
+      job();
+    });
 
     Worker { id, thread }
   }
 }
 
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
 pub struct ThreadPool {
   workers: Vec<Worker>,
+  sender: mpsc::Sender<Job>,
 }
 
 impl ThreadPool {
@@ -28,20 +40,27 @@ impl ThreadPool {
   pub fn new(size: usize) -> Self {
     assert!(size > 0);
 
+    let (sender, receiver) = mpsc::channel();
+
+    let receiver = Arc::new(Mutex::new(receiver));
+
     let mut workers = Vec::with_capacity(size);
 
     for id in 0..size {
-      let worker = Worker::new(id);
+      let worker = Worker::new(id, Arc::clone(&receiver));
 
       workers.push(worker);
     }
 
-    ThreadPool { workers }
+    ThreadPool { workers, sender }
   }
 
   pub fn execute<F>(&self, f: F)
   where
     F: FnOnce() + Send + 'static,
   {
+    let job = Box::new(f);
+
+    self.sender.send(job).unwrap();
   }
 }

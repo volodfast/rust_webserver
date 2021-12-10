fde9768252ad5f6b52d4a3d08545a84653c60d89
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-12-10 22:50:43 +0200

    Make Worker struct private

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 use std::thread;
 
-pub struct Worker {
+struct Worker {
   id: usize,
   thread: thread::JoinHandle<()>,
 }
 
 impl Worker {
-  pub fn new(id: usize) -> Self {
+  fn new(id: usize) -> Self {
     let thread = thread::spawn(|| {});
 
     Worker { id, thread }
